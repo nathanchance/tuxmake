b51a8f60a2d54373e933addaf325c69284e941da
Antonio Terceiro <antonio.terceiro@linaro.org>
2020-09-28 17:42:53 -0300

    Split exceptions into user and infrastructure errors
    Closes #75

--- tuxmake/exceptions.py
+++ tuxmake/exceptions.py
@@ -7,55 +7,74 @@ class TuxMakeException(Exception):
             return name
 
 
-class UnrecognizedSourceTree(TuxMakeException):
+class TuxMakeInfrastructureError(TuxMakeException):
+    """
+    Subclasss of this exception type are raised when tuxmake can't recover from
+    a situation caused by an error in tuxmake itself, or a failure in
+    infrastructure that tuxmake is using to perform a build.
+    """
+
+    pass
+
+
+class TuxMakeUserError(TuxMakeException):
+    """
+    Subclasss of this exception type are raised when tuxmake can't recover from
+    invalid user input, or invalid build options.
+    """
+
+    pass
+
+
+class UnrecognizedSourceTree(TuxMakeUserError):
     msg = "{name} does not look like a Linux source tree"
 
 
-class UnsupportedTarget(TuxMakeException):
+class UnsupportedTarget(TuxMakeUserError):
     msg = "Unsupported target: {name}"
     pass
 
 
-class UnsupportedArchitecture(TuxMakeException):
+class UnsupportedArchitecture(TuxMakeUserError):
     msg = "Unsupported architecture: {name}"
     pass
 
 
-class UnsupportedToolchain(TuxMakeException):
+class UnsupportedToolchain(TuxMakeUserError):
     msg = "Unsupported toolchain: {name}"
     pass
 
 
-class UnsupportedWrapper(TuxMakeException):
+class UnsupportedWrapper(TuxMakeUserError):
     msg = "Unsupported compiler wrapper: {name}"
     pass
 
 
-class UnsupportedKconfig(TuxMakeException):
+class UnsupportedKconfig(TuxMakeUserError):
     msg = "Unsupported kconfig: {name}"
 
 
-class InvalidKConfig(TuxMakeException):
+class InvalidKConfig(TuxMakeUserError):
     msg = "Invalid kconfig: {name}"
 
 
-class UnsupportedKconfigFragment(TuxMakeException):
+class UnsupportedKconfigFragment(TuxMakeUserError):
     msg = "Unsupported kconfig fragment: {name}"
 
 
-class InvalidRuntimeError(TuxMakeException):
+class InvalidRuntimeError(TuxMakeUserError):
     msg = "Invalid runtime: {name}"
 
 
-class RuntimePreparationFailed(TuxMakeException):
+class RuntimePreparationFailed(TuxMakeInfrastructureError):
     msg = "Runtime preparation failed: {name}"
 
 
-class UnsupportedMetadata(TuxMakeException):
+class UnsupportedMetadata(TuxMakeInfrastructureError):
     msg = "Unsupported metadata extractor: {name}"
     pass
 
 
-class UnsupportedMetadataType(TuxMakeException):
+class UnsupportedMetadataType(TuxMakeInfrastructureError):
     msg = "Unsupported metadata type: {name}"
     pass

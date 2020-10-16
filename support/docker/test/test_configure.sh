25263f8eca9ae17ebfc0261066c173ca89b7f6c0
Antonio Terceiro <antonio.terceiro@linaro.org>
2020-10-16 14:27:44 -0300

    docker: include llvm package in clang images

--- support/docker/test/test_configure.sh
+++ support/docker/test/test_configure.sh
@@ -76,18 +76,18 @@ test_x86_64_gcc() {
 
 test_clang() {
     get_build_args clang
-    assertArg 'BASE=$(PROJECT)/base' 'PACKAGES="clang-10 lld-10"'
+    assertArg 'BASE=$(PROJECT)/base' 'PACKAGES="clang-10 llvm-10 lld-10"'
 }
 
 test_arm64_clang() {
     get_build_args arm64_clang
     assertArg 'BASE=$(PROJECT)/clang' 'HOSTARCH=aarch64' \
-        'PACKAGES="clang-10 lld-10 gcc-aarch64-linux-gnu g++-aarch64-linux-gnu"'
+        'PACKAGES="clang-10 llvm-10 lld-10 gcc-aarch64-linux-gnu g++-aarch64-linux-gnu"'
 }
 
 test_clang10() {
     get_build_args clang-10
-    assertArg 'BASE=$(PROJECT)/base' 'PACKAGES="clang-10 lld-10"'
+    assertArg 'BASE=$(PROJECT)/base' 'PACKAGES="clang-10 llvm-10 lld-10"'
 }
 
 . shunit2

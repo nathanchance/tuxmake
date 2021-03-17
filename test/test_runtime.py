f035656f583fdd895f47c460b741cf86c3b5afb3
Antonio Terceiro <antonio.terceiro@linaro.org>
2021-03-17 10:35:25 -0300

    runtime: docker/podman: run all commands on a single container
    With this, the sccache daemon will persist through the entire build, and
    we will be able to take statistics from it. It should also provide a
    small performance improvement, since the overhead of starting a
    container is now only paid once per build.
    
    Fixes #79

--- test/test_runtime.py
+++ test/test_runtime.py
@@ -70,7 +70,20 @@ def q(img):
     return f"{DEFAULT_CONTAINER_REGISTRY}/{img}"
 
 
-class TestDockerRuntime:
+@pytest.fixture
+def container_id():
+    return "0123456789abcdef"
+
+
+class TestContainerRuntime:
+    @pytest.fixture(autouse=True)
+    def spawn_container(self, mocker, container_id):
+        return mocker.patch(
+            "tuxmake.runtime.DockerRuntime.spawn_container", return_value=container_id
+        )
+
+
+class TestDockerRuntime(TestContainerRuntime):
     def test_image(self, build, get_image):
         get_image.return_value = "foobarbaz"
         runtime = DockerRuntime()
@@ -134,35 +147,53 @@ class TestDockerRuntime:
         PodmanRuntime().prepare(build)
         assert len(check_call.call_args_list) == 2
 
+    def test_start_container(self, build, container_id):
+        runtime = DockerRuntime()
+        runtime.start_container(build)
+        assert runtime.container_id == container_id
+
+    def test_cleanup(self, build, container_id, mocker):
+        check_call = mocker.patch("subprocess.check_call")
+        runtime = DockerRuntime()
+        runtime.start_container(build)
+        runtime.cleanup()
+        cmd = check_call.call_args[0][0]
+        assert cmd[0:2] == ["docker", "stop"]
+        assert cmd[-1] == container_id
+
     def test_get_command_line(self, build):
         cmd = DockerRuntime().get_command_line(build, ["date"], False)
-        assert cmd[0:2] == ["docker", "run"]
+        assert cmd[0:2] == ["docker", "exec"]
         assert cmd[-1] == "date"
 
-    def test_environment(self, build):
+    def test_environment(self, build, spawn_container):
         build.environment = {"FOO": "BAR"}
-        cmd = DockerRuntime().get_command_line(build, ["date"], False)
+        DockerRuntime().start_container(build)
+        cmd = spawn_container.call_args[0][0]
         assert "--env=FOO=BAR" in cmd
 
-    def test_ccache(self, build, home):
+    def test_ccache(self, build, home, spawn_container):
         ccache = Wrapper("ccache")
         orig_ccache_dir = ccache.environment["CCACHE_DIR"]
         build.wrapper = ccache
-        cmd = DockerRuntime().get_command_line(build, ["date"], False)
+        DockerRuntime().start_container(build)
+        cmd = spawn_container.call_args[0][0]
         assert "--env=CCACHE_DIR=/ccache-dir" in cmd
         assert f"--volume={orig_ccache_dir}:/ccache-dir" in cmd
 
-    def test_sccache_with_path(self, build, home):
+    def test_sccache_with_path(self, build, home, spawn_container):
         sccache_from_host = Wrapper("/opt/bin/sccache")
         build.wrapper = sccache_from_host
-        cmd = DockerRuntime().get_command_line(build, ["date"], False)
+        DockerRuntime().start_container(build)
+        cmd = spawn_container.call_args[0][0]
         assert "--volume=/opt/bin/sccache:/usr/local/bin/sccache" in cmd
 
-    def test_TUXMAKE_DOCKER_RUN(self, build, monkeypatch):
+    def test_TUXMAKE_DOCKER_RUN(self, build, monkeypatch, spawn_container):
         monkeypatch.setenv(
             "TUXMAKE_DOCKER_RUN", "--hostname=foobar --env=FOO='bar baz'"
         )
-        cmd = DockerRuntime().get_command_line(build, ["bash"], False)
+        DockerRuntime().start_container(build)
+        cmd = spawn_container.call_args[0][0]
         assert "--hostname=foobar" in cmd
         assert "--env=FOO=bar baz" in cmd
 
@@ -199,7 +230,19 @@ class TestDockerRuntime:
         assert str(DockerRuntime()) == "docker"
 
 
-class TestDockerLocalRuntime:
+class TestDockerRuntimeSpawnContainer:
+    def test_spawn_container(self, build, mocker, container_id):
+        check_output = mocker.patch(
+            "subprocess.check_output", return_value=container_id
+        )
+        runtime = DockerRuntime()
+        runtime.start_container(build)
+        cmd = check_output.call_args[0][0]
+        assert cmd[0:2] == ["docker", "run"]
+        assert runtime.container_id == container_id
+
+
+class TestDockerLocalRuntime(TestContainerRuntime):
     def test_prepare_checks_local_image(self, build, get_image, mocker):
         get_image.return_value = "mylocalimage"
         check_call = mocker.patch("subprocess.check_call")
@@ -231,7 +274,7 @@ class TestDockerLocalRuntime:
         assert str(DockerLocalRuntime()) == "docker-local"
 
 
-class TestPodmanRuntime:
+class TestPodmanRuntime(TestContainerRuntime):
     def test_prepare(self, build, get_image, mocker):
         get_image.return_value = "myimage"
         check_call = mocker.patch("subprocess.check_call")
@@ -240,38 +283,42 @@ class TestPodmanRuntime:
 
     def test_get_command_line(self, build):
         cmd = PodmanRuntime().get_command_line(build, ["date"], False)
-        assert cmd[0:2] == ["podman", "run"]
+        assert cmd[0:2] == ["podman", "exec"]
         assert cmd[-1] == "date"
 
     def test_listed_as_supported(self):
         assert "podman" in Runtime.supported()
 
-    def test_no_user_option(self, build):
-        cmd = PodmanRuntime().get_command_line(build, ["date"], False)
+    def test_no_user_option(self, build, spawn_container):
+        PodmanRuntime().start_container(build)
+        cmd = spawn_container.call_args[0][0]
         assert len([c for c in cmd if "--user=" in c]) == 0
 
     def test_str(self):
         assert str(PodmanRuntime()) == "podman"
 
-    def test_TUXMAKE_PODMAN_RUN(self, build, monkeypatch):
+    def test_TUXMAKE_PODMAN_RUN(self, build, monkeypatch, spawn_container):
         monkeypatch.setenv(
             "TUXMAKE_PODMAN_RUN", "--hostname=foobar --env=FOO='bar baz'"
         )
-        cmd = PodmanRuntime().get_command_line(build, ["bash"], False)
+        PodmanRuntime().start_container(build)
+        cmd = spawn_container.call_args[0][0]
         assert "--hostname=foobar" in cmd
         assert "--env=FOO=bar baz" in cmd
 
-    def test_selinux_label(self, build):
-        cmd = PodmanRuntime().get_command_line(build, ["bash"], False)
+    def test_selinux_label(self, build, spawn_container):
+        PodmanRuntime().start_container(build)
+        cmd = spawn_container.call_args[0][0]
         volumes = [o for o in cmd if o.startswith("--volume=")]
         assert all([v.endswith(":z") for v in volumes])
 
-    def test_logging_level(self, build):
-        cmd = PodmanRuntime().get_command_line(build, ["bash"], False)
+    def test_logging_level(self, build, spawn_container):
+        PodmanRuntime().start_container(build)
+        cmd = spawn_container.call_args[0][0]
         assert "--log-level=ERROR" in cmd
 
 
-class TestPodmanLocalRuntime:
+class TestPodmanLocalRuntime(TestContainerRuntime):
     def test_prepare_checks_local_image(self, build, get_image, mocker):
         get_image.return_value = "mylocalimage"
         check_call = mocker.patch("subprocess.check_call")

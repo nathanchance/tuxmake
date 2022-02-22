4873a829233336959556a2ab66647e2a4af1bacb
Antonio Terceiro <antonio.terceiro@linaro.org>
2022-02-22 15:11:49 -0300

    dtbs-legacy: fix preconditions to match arm64 on 4.19 and older
    Fixes: 31d2bcfb5cb4 ("target: dtbs-legacy: skip in the absence of a `dtbs` Makefile target")
    Reported-by: Naresh Kamboju <naresh.kamboju@linaro.org>
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- test/test_build.py
+++ test/test_build.py
@@ -587,6 +587,7 @@ class TestDtbsLegacy:
         subprocess.check_call(
             ["sed", "-i", "-e", "s/dtbs_install/XXXX/g", str(linux_rw / "Makefile")]
         )
+        (linux_rw / "arch/arm64/Makefile").write_text("dtbs:\n\t@echo build dtbs\n")
         return linux_rw
 
     def test_collect_dtbs_manually_without_dtbs_install(self, oldlinux):

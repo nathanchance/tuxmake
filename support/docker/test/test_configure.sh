nathanchance/tuxmake

commit 2d714fb37e03189bce3cf4c6043a3c016d0fc026

images: ensure 32-bit ARM toolchain is included in native arm64

support/docker/configure
@@ -129,6 +129,17 @@ class BuildImage(Image):
         super().__init__(image_data)
         self.pkgname = self.name
 
+    @property
+    def arch(self):
+        try:
+            return getattr(self, "__arch__")
+        except AttributeError:
+            a = re.sub(r"_(rust|gcc|clang).*$", "", self.name)
+            if a == self.name:
+                return None
+            self.__arch__ = Architecture(a)
+            return self.__arch__
+
     def emit(self):
         super().emit()
         packages = " ".join(self.get_packages())
@@ -142,23 +153,22 @@ class BuildImage(Image):
 
 
 class ClangBuildImage(BuildImage):
-    pass
+    def get_packages(self):
+        extra_packages = []
+        if self.arch == "arm64":
+            extra_packages = ["binutils-arm-linux-gnueabihf"]
+        return super().get_packages() + extra_packages
 
 
 class GccBuildImage(BuildImage):
-    pass
+    def get_packages(self):
+        extra_packages = []
+        if self.arch == "arm64":
+            extra_packages = [f"{pkg}-arm-linux-gnueabihf" for pkg in self.packages]
+        return super().get_packages() + extra_packages
 
 
 class CrossBuildMixin:
-    @property
-    def arch(self):
-        try:
-            return getattr(self, "__arch__")
-        except AttributeError:
-            a = re.sub(r"_(rust|gcc|clang).*$", "", self.name)
-            self.__arch__ = Architecture(a)
-            return self.__arch__
-
     @property
     def lists(self):
         arch = self.arch.name
@@ -185,7 +195,7 @@ class CrossBuildMixin:
             self.test_args.append("--libc-compile-fatal")
 
 
-class CrossGccBuildImage(CrossBuildMixin, BuildImage):
+class CrossGccBuildImage(CrossBuildMixin, GccBuildImage):
     extra_lists = ["crossbuild"]
 
     def __init__(self, image_data):
@@ -193,23 +203,17 @@ class CrossGccBuildImage(CrossBuildMixin, BuildImage):
         self.pkgname = re.sub(r".*_(gcc.*)$", "\g<1>", self.name)
 
     def get_packages(self):
-        packages = self.packages.copy()
-        suffixes = [self.cross_package_suffix]
-        if self.cross_package_suffix == "aarch64-linux-gnu":
-            suffixes.append("arm-linux-gnueabihf")
-        for suffix in suffixes:
-            packages += [pkg + "-" + suffix for pkg in self.packages]
-        return packages
+        return super().get_packages() + [
+            pkg + "-" + self.cross_package_suffix
+            for pkg in self.packages
+        ]
 
 
 class CrossClangBuildImage(CrossBuildMixin, ClangBuildImage):
     extra_lists = ["crossbuild"]
 
     def get_packages(self):
-        binutils = ["binutils-" + self.cross_package_suffix]
-        if self.cross_package_suffix == "aarch64-linux-gnu":
-            binutils.append("binutils-arm-linux-gnueabihf")
-        return self.packages + binutils
+        return super().get_packages() + ["binutils-" + self.cross_package_suffix]
 
 
 class ClangAndroidBuildImage(ClangBuildImage):

support/docker/test/test_configure.sh
@@ -55,13 +55,13 @@ test_gcc_9() {
 test_arm64_gcc() {
     get_build_args arm64_gcc
     assertArg 'BASE=$(REGISTRY)$(PROJECT)/gcc' 'HOSTARCH=aarch64'\
-        'PACKAGES="gcc g++ gcc-aarch64-linux-gnu g++-aarch64-linux-gnu gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf"'
+        'PACKAGES="gcc g++ gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf gcc-aarch64-linux-gnu g++-aarch64-linux-gnu"'
 }
 
 test_arm64_gcc_8() {
     get_build_args arm64_gcc-8
     assertArg 'BASE=$(REGISTRY)$(PROJECT)/gcc' 'HOSTARCH=aarch64'\
-        'PACKAGES="gcc-8 g++-8 gcc-8-aarch64-linux-gnu g++-8-aarch64-linux-gnu gcc-8-arm-linux-gnueabihf g++-8-arm-linux-gnueabihf"'
+        'PACKAGES="gcc-8 g++-8 gcc-8-arm-linux-gnueabihf g++-8-arm-linux-gnueabihf gcc-8-aarch64-linux-gnu g++-8-aarch64-linux-gnu"'
 }
 
 test_x86_64_gcc() {
@@ -78,7 +78,7 @@ test_clang() {
 test_arm64_clang() {
     get_build_args arm64_clang
     assertArg 'BASE=$(REGISTRY)$(PROJECT)/clang' 'HOSTARCH=aarch64' \
-        'PACKAGES="clang llvm lld binutils-aarch64-linux-gnu binutils-arm-linux-gnueabihf"'
+        'PACKAGES="clang llvm lld binutils-arm-linux-gnueabihf binutils-aarch64-linux-gnu"'
 }
 
 test_clang_11() {

tuxmake/runtime/bin/tuxmake-check-environment
@@ -141,6 +141,7 @@ sub check_toolchain_image() {
   $arch ||= `arch`; chomp $arch;
   my @prefixes = ("");
   push @prefixes, $cross_compile if $cross_compile;
+  push @prefixes, "arm-linux-gnueabihf-" if ($arch eq "arm64");
 
   for $compiler (@compilers) {
     for my $prefix (@prefixes) {

commit 20741021b5e66b99a718eeb5465947888a687c47

images: fix installation of packages on native images
When building the arm64 build images on arm64 hosts, the extra packages
for arm-linux-gnueabihf where not being installed because they were
being handled together with the "normal" cross compilers for arm64,
which don't need (and can't) be installed because gcc-aarch64-linux-gnu
is not available on arm64 since that is provided by the regular gcc
package.

Stop skipping the installation in the Dockerfile when the host
architecture is different from the native one, and instead compose the
list of packages to install differently based on that, and install
whatever list is obtained unconditionally.

Fixes: 2d714fb37e03 ("images: ensure 32-bit ARM toolchain is included in native arm64")

support/docker/Dockerfile.build
@@ -22,14 +22,12 @@ RUN if [ -f /etc/apt/sources.list.d/extra.list ]; then exit; fi; \
 
 # Install basic toolchain packages. Which toolchain exactly is defined by the
 # $PACKAGES build argument
-RUN if [ "$(uname -m)" != "${HOSTARCH}" ]; then apt-get update \
-    && apt-get install \
+RUN apt-get install \
         --assume-yes \
         --no-install-recommends \
         --option=debug::pkgProblemResolver=yes \
         ${INSTALL_OPTIONS} \
-        $PACKAGES; \
-    fi
+        $PACKAGES
 
 # Install extra development packages
 #

support/docker/configure
@@ -11,7 +11,7 @@ sys.path.insert(0, str(tuxmake))
 
 
 from tuxmake.runtime import NullRuntime, DockerRuntime
-from tuxmake.arch import Architecture
+from tuxmake.arch import Architecture, native_arch
 
 TAG = os.getenv("TAG")
 
@@ -203,10 +203,13 @@ class CrossGccBuildImage(CrossBuildMixin, GccBuildImage):
         self.pkgname = re.sub(r".*_(gcc.*)$", "\g<1>", self.name)
 
     def get_packages(self):
-        return super().get_packages() + [
-            pkg + "-" + self.cross_package_suffix
-            for pkg in self.packages
-        ]
+        extra_packages = []
+        if self.arch != native_arch.name:
+            extra_packages = [
+                pkg + "-" + self.cross_package_suffix
+                for pkg in self.packages
+            ]
+        return super().get_packages() + extra_packages
 
 
 class CrossClangBuildImage(CrossBuildMixin, ClangBuildImage):

support/docker/test/test_configure.sh
@@ -67,7 +67,7 @@ test_arm64_gcc_8() {
 test_x86_64_gcc() {
     get_build_args x86_64_gcc
     assertArg 'BASE=$(REGISTRY)$(PROJECT)/gcc' 'HOSTARCH=x86_64'\
-        'PACKAGES="gcc g++ gcc-x86-64-linux-gnu g++-x86-64-linux-gnu"'
+        'PACKAGES="gcc g++"'
 }
 
 test_clang() {
@@ -105,5 +105,10 @@ test_clang_all_includes_only_glang_images_not_clang_N() {
         echo ' \------------------------------------------------'
     fi
 }
+
+if [ "$(uname -m)" != "x86_64" ]; then
+    echo "I: skipping tests, as they only apply on x86_64"
+    exit
+fi
 
 . /usr/share/shunit2/shunit2